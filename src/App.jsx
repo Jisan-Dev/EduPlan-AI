c6615e7dca39d473a72e25ed643b6df14a99ba75
Jisun696 <istiakkashem35@gmail.com>
2025-02-22 17:18:32 +0600

    initialize project and create the AuthContext

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Button className="ml-1.5">button</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useContext } from "react";
+import { createContext } from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem("isAuthenticated") === "true");
+
+  const login = (email, password) => {
+    if (email === "demouser" && password === "demopass") {
+      setIsAuthenticated(true);
+      localStorage.setItem("isAuthenticated", "true");
+      return true;
+    }
+    return false;
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    localStorage.setItem("isAuthenticated", "false");
+  };
+
+  return <AuthContext.Provider value={{ isAuthenticated, login, logout }}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  return context;
+};
