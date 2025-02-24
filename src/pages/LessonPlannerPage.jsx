9dd0852504c86d87fe9c1fed3eb8d1ec9a96bb19
Jisun696 <istiakkashem35@gmail.com>
2025-02-24 21:51:58 +0600

    Add theme switch feature

--- src/pages/LessonPlannerPage.jsx
+++ src/pages/LessonPlannerPage.jsx
@@ -1,4 +1,5 @@
 import LessonForm from "@/components/lesson/LessonForm";
+import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/context/AuthContext";
 import { useNavigate } from "react-router";
@@ -14,20 +15,23 @@ const LessonPlannerPage = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50">
-      <header className="bg-white shadow">
+    <div className="min-h-screen">
+      <header className="shadow bg-accent">
         <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-          <h1 className="text-xl font-bold">AI Lesson Planner</h1>
-          <Button variant="outline" onClick={logout}>
-            Logout
-          </Button>
+          <h1 className="text-xl text-accent-foreground font-bold">EduPlan-AI Lesson Planner</h1>
+          <div className="flex items-center justify-center gap-3">
+            <Button variant="default" onClick={logout} className="cursor-pointer">
+              Logout
+            </Button>
+            <ModeToggle />
+          </div>
         </div>
       </header>
       <main className="container mx-auto px-4 py-8">
         <LessonForm />
       </main>
-      <footer className="bg-white border-t py-4 mt-8">
-        <div className="container mx-auto px-4 text-center text-gray-500">&copy; {new Date().getFullYear()} AI Lesson Planner</div>
+      <footer className="bg-accent border-t py-4 mt-8">
+        <div className="container mx-auto px-4 text-center text-accent-foreground">&copy; {new Date().getFullYear()} AI Lesson Planner</div>
       </footer>
     </div>
   );
